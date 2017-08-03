fa6ab7f69d65e0e637e1be69f5332e262c02cbe6
Juan Antonio <ruchin10@gmail.com>
2017-08-03 19:37:00 +0200

    developed some methods like 'train'

--- kMeansClassifier.py
+++ kMeansClassifier.py
@@ -1,6 +1,9 @@
+import random
+import KMeansCluster from KMeansCluster
+from numpy import *
 
 class KMeansClassifier:
-    classes = []
+    clusters = []
 
     def addGroup(self, group):
         self.groups.append(group)
@@ -8,5 +11,45 @@ class KMeansClassifier:
     def getGroups(self):
         return self.groups
 
-    def test(self, numClasses, dataset):
-        
+    def train(self, numClusters, dataset):
+        # create the cluster objects
+        self.createInitClusters(self, randomClusters, dataset)
+
+        # training
+        anyClusterHasMoved = True
+        while anyClusterHasMoved:
+            # for each point, find nearest cluster
+            for cluster in clusters:
+                clusterPoints = cluster.getPoints()
+                for point in clusterPoints:
+                    closerCluster = self.getCloserCluster(self, point)
+                    if cluster.isNot(closerCluster):
+
+
+    def createInitClusters(self, numClusters, dataset):
+        # get all the N random clusters
+        randomClusters = random.sample(range(0,len(dataset)), numClusters)
+
+        for example in randomClusters:
+            randomCluster = new KMeansCluster()
+            randomCluster.init(dataset[example])
+            self.cluster.append(randomCluster)
+
+        for point in dataset:
+            cluster = self.getCloserCluster(self, point)
+            cluster.addPoint(point)
+
+    def getCloserCluster(self, point):
+        minDistance = -1
+        closer = new KMeansCluster()
+
+        for cluster in clusters:
+            distance = self.getDistance(self, point, cluster)
+            if (minDistance == -1) || distance < minDistance:
+                closer = cluster
+                minDistance = distance
+
+        return closer
+
+    def getDistance(self, point, cluster):
+        return sqrt(sum(power(point - cluster.getPosition(), 2)))
